d0ae6e2e06d98ec0f85ae7a8473d6f766cdc12fc
Aiden Yang <aidenyang@Aidens-MacBook-Air.local>
2020-12-27 15:20:24 +0800

    initial setup

---
+++ devback/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/api/message', (req, res) => {
+  res.send('hello this is from backend');
+});
+
+app.listen(4000, () => {
+  console.log('listening on 4000');
+});
